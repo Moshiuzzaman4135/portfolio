7087a8ae3d6362d5b9d4d0fa20371d1dad253892
Moshiuzzaman4135 <shatil4135@gmail.com>
2025-10-21 23:37:19 +0600

    "Debug BASE_PATH value in build"

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 const basePath = process.env.BASE_PATH ?? '/';
 
+console.log(">>> BUILD BASE_PATH =", process.env.BASE_PATH);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: basePath,

51fec641b30743faf9193439c355ff31f0640ef4
Moshiuzzaman Shatil <55358217+Moshiuzzaman4135@users.noreply.github.com>
2025-10-22 00:56:26 +0600

    Improve global scroll restoration

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,21 @@
+import { useLayoutEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export function ScrollToTop() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+  }, []);
+
+  useLayoutEffect(() => {
+    const scrollableRoot = document.scrollingElement ?? document.documentElement;
+
+    scrollableRoot.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    document.body.scrollTop = 0;
+  }, [location.pathname, location.search, location.hash]);
+
+  return null;
+}

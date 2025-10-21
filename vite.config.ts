e4a0b51684a6abb58ebfb419601f6d122d369f94
Moshiuzzaman4135 <shatil4135@gmail.com>
2025-10-21 23:41:08 +0600

    Revert ""Debug BASE_PATH value in build""
    This reverts commit 7087a8ae3d6362d5b9d4d0fa20371d1dad253892.

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 const basePath = process.env.BASE_PATH ?? '/';
 
-console.log(">>> BUILD BASE_PATH =", process.env.BASE_PATH);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: basePath,
